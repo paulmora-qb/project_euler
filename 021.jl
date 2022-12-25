e52c8300861b9a06bec8982cea15b4952f548394
PaulMora <96173496+paulmora-qb@users.noreply.github.com>
2022-12-25 01:15:41 -0500

    solving 2 more

---
+++ 021.jl
@@ -0,0 +1,24 @@
+
+using Primes: factor
+using Combinatorics: combinations
+
+
+function amicable_number(number)
+    combinations_primes = Set(collect(combinations(factor(Vector, number))))
+    products = append!([prod(x) for x in combinations_primes], 1)
+    products = filter!(x -> x ≠ number, products)
+    return sum(products)
+end
+
+function problem21()
+    sum_of_amicable_numbers = 0
+    for i in 1:(10_000)
+        an_result = amicable_number(i)
+        if i == amicable_number(an_result)
+            sum_of_amicable_numbers += i
+        end
+    end
+    return sum_of_amicable_numbers
+end
+
+problem21()
